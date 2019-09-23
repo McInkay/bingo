21e8b5727c5649a9e64f7c8e692458a23ef304e0
Al McKinlay <github@10people.co.uk>
2019-09-23 12:05:11 +0100

    Add option to view US sales as well

--- switch/src/App.js
+++ switch/src/App.js
@@ -1,45 +1,44 @@
 import React, {useState} from 'react';
 import './App.css';
-import fetchJsonp from 'fetch-jsonp';
-const PRICE = {};
-PRICE[250] = "price_lowest_f%3A%220%22%20OR%20price_lowest_f%3A%5B0.01%20TO%202.49%5D";
-PRICE[500] = PRICE[250] + "%20OR%20price_lowest_f%3A%5B2.5%20TO%204.99%5D";
+import {regions, REGIONS} from './regions';
 
-const getData = async () => {
-
-  const url = (num) => `https://searching.nintendo-europe.com/en/select?q=*&fq=type%3AGAME%20AND%20((price_has_discount_b%3A"true")%20AND%20(${PRICE[500]}))%20AND%20sorting_title%3A*%20AND%20*%3A*&sort=price_sorting_f%20asc&start=${num*24}&rows=24&wt=json&bf=linear(ms(priority%2CNOW%2FHOUR)%2C1.1e-11%2C0)&json.wrf=callback${num}`;
-
-  const promise = (num) => fetchJsonp(url(num), {
-    jsonpCallback: 'callback' + num,
-    jsonpCallbackFunction: 'callback' + num
-  }).then((response) => response.json());
-
+const getData = async (regionKey) => {
   const prices = [];
+  const region = regions[regionKey];
+  const regionFetch = (num) => region.fetch(num, region.getURL, region.method, region.getBody);
 
-  return promise(0).then((data) => {
+  return regionFetch(0).then((data) => {
       const promises = [];
       let i = 0;
-      while(i * 24 < data.response.numFound) {
-          promises.push(promise(i));
+      while(i * 24 < region.getNumHits(data)) {
+          promises.push(regionFetch(i));
           i++;
       }
       return Promise.all(promises);
   }).then(results => {
-      return results.reduce((arr, val) => arr.concat(val.response.docs), []);
+      return results.reduce((arr, val) => arr.concat(region.reduce(val)), []);
   }).then((data) => data.forEach(doc => {
-      prices.push({title: doc.title, price: doc.price_sorting_f, original: Math.round((doc.price_sorting_f / (100 - doc.price_discount_percentage_f) * 100)*100)/100, discount: doc.price_discount_percentage_f});
+      prices.push(region.map(doc));
   })).then(() => prices);
 
 }
+
 function App() {
   const [prices, setPrices] = useState([])
-  getData().then((newPrices) => newPrices.length !== prices.length && setPrices(newPrices));
-  console.table(prices)
+  const [region, setRegion] = useState(REGIONS.EU);
+  const changeRegion = (newRegion) => {
+    setRegion(newRegion);
+    setPrices([]);
+  }
+  getData(region).then((newPrices) => newPrices.length !== prices.length && setPrices(newPrices));
   return (
     <div className="App">
       <header className="App-header">
         Switch sales
       </header>
+      <select value={region} onChange={(event) => changeRegion(event.target.value)}>
+        {Object.entries(REGIONS).map(([key, value]) => (<option value={value} key={key}>{value}</option>))}
+      </select>
       <table>
         <thead>
           <tr>
@@ -50,12 +49,12 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {prices.map((entry) => (<tr key={entry.title}>
+          {prices.length > 0 ? prices.map((entry) => (<tr key={entry.title}>
             <td>{entry.title}</td>
             <td>{entry.price}</td>
             <td>{entry.original}</td>
             <td>{entry.discount}%</td>
-          </tr>))}
+          </tr>)) : "Loading"}
         </tbody>
       </table>
     </div>
---
+++ switch/src/regions.js
@@ -0,0 +1,40 @@
+import fetchJsonp from 'fetch-jsonp';
+
+export const REGIONS = {
+    EU: "Europe",
+    NA: "USA"
+}
+
+const PRICE = {};
+PRICE[250] = "price_lowest_f%3A%220%22%20OR%20price_lowest_f%3A%5B0.01%20TO%202.49%5D";
+PRICE[500] = PRICE[250] + "%20OR%20price_lowest_f%3A%5B2.5%20TO%204.99%5D";
+
+export const regions = {
+    [REGIONS.EU]: {
+        fetch: (num, getUrl) => fetchJsonp(getUrl(num), {
+            jsonpCallback: 'callback' + num,
+            jsonpCallbackFunction: 'callback' + num
+          }).then((response) => response.json()),
+        getURL: (num) => `https://searching.nintendo-europe.com/en/select?q=*&fq=type%3AGAME%20AND%20((price_has_discount_b%3A"true")%20AND%20(${PRICE[500]}))%20AND%20sorting_title%3A*%20AND%20*%3A*&sort=price_sorting_f%20asc&start=${num*24}&rows=24&wt=json&bf=linear(ms(priority%2CNOW%2FHOUR)%2C1.1e-11%2C0)&json.wrf=callback${num}`,
+        method: "GET",
+        getNumHits: (data) => data.response.numFound,
+        reduce: (val) => val.response.docs,
+        map: (game) => ({title: game.title, price: game.price_sorting_f, original: Math.round((game.price_sorting_f / (100 - game.price_discount_percentage_f) * 100)*100)/100, discount: game.price_discount_percentage_f})
+    },
+    [REGIONS.NA]: {
+        fetch: (num, getUrl, method, getBody) => fetch(getUrl(num), {method, body: JSON.stringify(getBody(num))}).then((res) => res.json()),
+        getURL: () => `https://u3b6gr4ua3-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(3.33.0)%3B%20Browser%20(lite)%3B%20JS%20Helper%202.20.1&x-algolia-application-id=U3B6GR4UA3&x-algolia-api-key=9a20c93440cf63cf1a7008d75f7438bf`,
+        method: "POST",
+        getBody: (num) => ({
+            "requests": [
+                {
+                    "indexName": "noa_aem_game_en_us_price_asc",
+                    "params": `query=&hitsPerPage=24&maxValuesPerFacet=30&page=${num}&facets=%5B%22generalFilters%22%2C%22platform%22%2C%22availability%22%2C%22categories%22%2C%22filterShops%22%2C%22virtualConsole%22%2C%22characters%22%2C%22priceRange%22%2C%22esrb%22%2C%22filterPlayers%22%5D&tagFilters=&facetFilters=%5B%5B%22platform%3ANintendo%20Switch%22%5D%2C%5B%22generalFilters%3ADeals%22%5D%5D`
+                }
+            ]
+        }),
+        getNumHits: (data) => data.results[0].nbHits,
+        reduce: (val) => val.results[0].hits,
+        map: (game) => ({title: game.title, price: game.salePrice, original: game.msrp, discount: ((game.msrp - game.salePrice) / game.msrp * 100).toFixed(2)})
+    }
+}
