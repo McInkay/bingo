31924751e0e6a033b41735f66f54a33bf20fbb68
Al McKinlay <github@10people.co.uk>
2023-08-03 17:44:13 +0100

    [mirror] 4 triangles for mirror

--- mirror/src/App.js
+++ mirror/src/App.js
@@ -1,21 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import Canvas from "./Canvas";
+import ReactSlider from "react-slider";
 
 function App() {
+	const [rotate, setRotate] = useState(0);
 	return (
 		<div className="App">
 			<header className="App-header">Mirror</header>
+			<ReactSlider
+				className="horizontal-slider"
+				thumbClassName="example-thumb"
+				trackClassName="example-track"
+				max={360}
+				onChange={(value) => setRotate(value)}
+				renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+			/>
 			<div
 				style={{
 					display: "grid",
-					gridTemplateColumns: "50px 50px",
-					gridTemplateRows: "50px 50px",
+					gridTemplateColumns: "400px 400px",
+					gridTemplateRows: "300px 300px",
+					width: "1000px",
+					margin: "auto",
 				}}
 			>
-				<Canvas x={true} y={true} />
-				<Canvas x={false} y={true} />
-				<Canvas x={true} y={false} />
-				<Canvas x={false} y={false} />
+				<Canvas rotate={rotate + 0} />
+				<Canvas rotate={-rotate} />
+				<Canvas rotate={-rotate + 180} />
+				<Canvas rotate={rotate + 180} />
 			</div>
 		</div>
 	);
--- mirror/src/Canvas.js
+++ mirror/src/Canvas.js
@@ -1,26 +1,48 @@
 import React, { useRef, useEffect } from "react";
 
-const Canvas = ({ x, y, ...props }) => {
+const Canvas = (props) => {
 	const canvasRef = useRef(null);
-	console.log(x, y);
 
-	const draw = (ctx) => {
-		ctx.canvas.width = 50;
-		ctx.canvas.height = 50;
+	function drawTriangle(stage, size, x, y) {
+		var shape = new window.createjs.Shape(),
+			g = shape.graphics,
+			horizontal_offset = size / 10,
+			vertical_ratio = 0.866,
+			vertical_offset = (size * vertical_ratio) / 15;
+
+		g.setStrokeStyle(1);
+		g.beginStroke("green");
+		g.beginFill("green");
+
+		g.moveTo(0, -2 * vertical_offset);
+		g.lineTo(-1 * horizontal_offset, vertical_offset);
+		g.lineTo(horizontal_offset, vertical_offset);
+		g.lineTo(0, -2 * vertical_offset);
+
+		shape.x = x;
+		shape.y = y;
+
+		stage.addChild(shape);
+		shape.rotation = props.rotate;
+		stage.update();
+
+		return shape;
+	}
+
+	const draw = (ctx, stage) => {
+		ctx.canvas.width = 500;
+		ctx.canvas.height = 500;
 		ctx.clearRect(10, 10, ctx.canvas.width, ctx.canvas.height);
-		// ctx.scale(x ? 1 : -1, y ? 1 : -1);
-		ctx.fillStyle = "#000000";
-		ctx.beginPath();
-		ctx.arc(x ? 50 : 0, y ? 50 : 0, 25, 0, 2 * Math.PI);
-		ctx.fill();
+		drawTriangle(stage, 2000, 250, 250);
 	};
 
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		const context = canvas.getContext("2d");
+		const stage = new window.createjs.Stage(canvas);
 
 		//Our draw come here
-		draw(context);
+		draw(context, stage);
 	}, [draw]);
 
 	return <canvas ref={canvasRef} {...props} />;
